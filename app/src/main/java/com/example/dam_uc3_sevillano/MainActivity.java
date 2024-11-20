52bc41f2e629164b132dcecd9666b974851e8f0e
ariansevillano <ariansevillano1@gmail.com>
2024-11-20 17:32:54 -0500

    Ultimos camios 4to commit

--- app/src/main/java/com/example/dam_uc3_sevillano/MainActivity.java
+++ app/src/main/java/com/example/dam_uc3_sevillano/MainActivity.java
@@ -41,6 +41,30 @@ public class MainActivity extends AppCompatActivity {
         TextView tv4 = findViewById(R.id.textViewOneWay);
         tv4.setOnClickListener(view -> Toast.makeText(this, "Viaje solo de ida", Toast.LENGTH_SHORT).show());
 
+        TextView tv5 = findViewById(R.id.textView8);
+        tv5.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a Casa", Toast.LENGTH_SHORT).show());
+
+        TextView tv6 = findViewById(R.id.textView11);
+        tv6.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a Libro de Viajes", Toast.LENGTH_SHORT).show());
+
+        TextView tv7 = findViewById(R.id.textView14);
+        tv7.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a Mis Viajes", Toast.LENGTH_SHORT).show());
+
+        TextView tv8 = findViewById(R.id.textView12);
+        tv8.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a Notificaciones", Toast.LENGTH_SHORT).show());
+
+        TextView tv9 = findViewById(R.id.textView13);
+        tv9.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a Latam Pass", Toast.LENGTH_SHORT).show());
+
+        TextView tv10 = findViewById(R.id.textViewFlights);
+        tv10.setOnClickListener(view -> Toast.makeText(this, "Usted se encuentra en VUELOS", Toast.LENGTH_SHORT).show());
+
+        TextView tv11 = findViewById(R.id.textViewHotels);
+        tv11.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a HOTELES", Toast.LENGTH_SHORT).show());
+
+        TextView tv12 = findViewById(R.id.textViewCars);
+        tv12.setOnClickListener(view -> Toast.makeText(this, "Dirigiendo a AUTOS", Toast.LENGTH_SHORT).show());
+
 
 
         // Configurar selector de fecha
@@ -54,6 +78,10 @@ public class MainActivity extends AppCompatActivity {
                 String date = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
                 editTextDate.setText(date);
             }, year, month, day);
+
+            // Configurar fecha mínima (día actual)
+            datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+
             datePickerDialog.show();
         });
 
